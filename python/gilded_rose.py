6473fc57c954bb8397ba65a20006763ba9c9ec7f
jordan-lesaca <jordan.m.lesaca@gmail.com>
2025-10-20 20:37:38 -0700

    created methods check item is sulfuras increase quality decrease quality and is item and refactored out the update quality method to incorporate new methods and remove some repeated code.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -15,33 +15,48 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def check_item(self, item):
+        return item.sell_in < 0
+
+    def is_sulfuras(self, item):
+        return item.name == "Sulfuras, Hand of Ragnaros"
+
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality = min(50, item.quality + 1)
+
+    def decrease_quality(self, item):
+        if item.quality > 0:
+            item.quality = max(0, item.quality - 1)
+
+    def is_item(self, item): 
+        return item.name in ("Aged Brie", "Backstage passes to a TAFKAL80ETC concert")
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if not self.is_item(item):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    if not self.is_sulfuras(item):
+                        self.decrease_quality(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
+                self.increase_quality(item)
+                if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item.sell_in < 11:
+                        self.increase_quality(item)
+                    if item.sell_in < 6:
+                        self.increase_quality(item)
+
+            if not self.is_sulfuras(item):
+                item.sell_in -= 1
+
+            if self.check_item(item):
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            if not self.is_sulfuras(item):
+                                self.decrease_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increase_quality(item)
 
